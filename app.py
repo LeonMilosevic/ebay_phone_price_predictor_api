5d0f0ad91a19353ec05f2a1a3be42241db3699a9
LeonMilosevic <leonleon787@yahoo.com>
2021-01-13 20:03:02 +0200

    made skeleton

---
+++ app.py
@@ -0,0 +1,27 @@
+import pickle
+import json
+from flask import Flask, request
+from decouple import config
+from helpers import helper_func
+
+# secrets
+APP_MODEL_PATH = config("APP_MODEL_PATH")
+
+# load model from the file
+regressor = pickle.load(open(APP_MODEL_PATH, "rb"))
+
+
+app = Flask(__name__)
+
+@app.route("/predict", methods=["POST"])
+def predict() -> str:
+    processed_input = helpers.process_input(request.data)
+    
+    try:
+        prediction = regressor.predict(processed_input)
+    except:
+        return json.dumps({"error": "something went wrong, please try again"}), 500
+    return json.dumps({"Prediction": float(prediction[0])}), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ helpers/helper_func.py
@@ -0,0 +1,6 @@
+import numpy as np
+import json
+
+def process_input(input: str) -> np.array:
+    
+    print(json.loads(input))
